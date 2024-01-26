ef76df778b2297a766a35dc9fe518bc1d957006d
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-01-26 03:01:23 -0500

    Initial commit

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :episode_scheduler, EpisodeSchedulerWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "+pQxlMeAmd5nWJq87E3a9ySE6e+pUfrOgC0wNJmYvvS89gKRp+CroqS5IxXV1wT1",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ lib/episode_scheduler_web/router.ex
@@ -0,0 +1,11 @@
+defmodule EpisodeSchedulerWeb.Router do
+  use EpisodeSchedulerWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", EpisodeSchedulerWeb do
+    pipe_through :api
+  end
+end
