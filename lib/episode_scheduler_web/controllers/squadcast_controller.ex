0279d892b5f3976d328df0d1fd457921499ca8b7
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-02 14:35:15 -0500

    Creates juicy :tomato: red failing tests

---
+++ lib/episode_scheduler_web/controllers/squadcast_controller.ex
@@ -0,0 +1,7 @@
+defmodule EpisodeSchedulerWeb.SquadcastController do
+  use EpisodeSchedulerWeb, :controller
+
+  def create(conn, params) do
+  end
+
+end
--- test/episode_scheduler_web/controllers/squadcast_controller_test.exs
+++ test/episode_scheduler_web/controllers/squadcast_controller_test.exs
@@ -8,15 +8,18 @@ defmodule EpisodeSchedulerWeb.SquadcastControllerTest do
   @test_response EpisodeSchedulerWeb.Constants.get_test_squadcast_response
 
   describe "SquadCast controller" do
-    test "schedules a SquadCast recording given a valid session payload" do
-      valid_payload = @test_request
+    setup do
       # Mock a POST request to the SquadCast API
       Mox.expect(MockHTTPClient, :post, fn _url, _headers, _body ->
         {:ok, %{status_code: 200, body: @test_response}}
       end)
+      :ok
+    end
+    test "schedules a SquadCast recording given a valid session payload" do
       # call the controller action
-      EpisodeSchedulerWeb.SquadcastController.create(build_conn(), valid_payload)
-
+      conn = post(build_conn(), "/api/squadcast", @test_request)
+      # assert the response
+      assert json_response(conn, 200) == @test_response
 
     end
 
