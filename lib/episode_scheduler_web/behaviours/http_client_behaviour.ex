dd9cc49132847e036d13ebdedfaba08e4b09b08d
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-02 14:18:31 -0500

    Modifies test config and language

--- lib/episode_scheduler_web/behaviours/http_client_behaviour.ex
+++ lib/episode_scheduler_web/behaviours/http_client_behaviour.ex
@@ -1,4 +1,4 @@
-defmodule EpisodeSchedulerWeb.Behaviours.HttpClientBehaviour do
+defmodule EpisodeSchedulerWeb.Behaviours.HTTPClientBehaviour do
   @callback post(String.t, map, map) :: {:ok, map} | {:error, map}
   @callback get(String.t, map) :: {:ok, map} | {:error, map}
 end
--- test/episode_scheduler_web/controllers/squadcast_controller_test.exs
+++ test/episode_scheduler_web/controllers/squadcast_controller_test.exs
@@ -1,13 +1,21 @@
 defmodule EpisodeSchedulerWeb.SquadcastControllerTest do
   use EpisodeSchedulerWeb.ConnCase
 
+  alias EpisodeSchedulerWeb.MockHTTPClient
   alias EpisodeSchedulerWeb.SquadcastController
 
   @test_request EpisodeSchedulerWeb.Constants.get_test_squadcast_request
+  @test_response EpisodeSchedulerWeb.Constants.get_test_squadcast_response
 
-  describe "POST /api/squadcast" do
-    test "schedules a SquadCast recording given a valid payload" do
+  describe "SquadCast controller" do
+    test "schedules a SquadCast recording given a valid session payload" do
       valid_payload = @test_request
+      # Mock a POST request to the SquadCast API
+      Mox.expect(MockHTTPClient, :post, fn _url, _headers, _body ->
+        {:ok, %{status_code: 200, body: @test_response}}
+      end)
+      # call the controller action
+      EpisodeSchedulerWeb.SquadcastController.create(build_conn(), valid_payload)
 
 
     end
