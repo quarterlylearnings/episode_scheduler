0b260b3d096bdcbf1c0a2d2be0d1f087af5e6238
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-01 23:32:35 -0500

    Punts on empty payload test

--- lib/episode_scheduler_web/controllers/calendly_controller.ex
+++ lib/episode_scheduler_web/controllers/calendly_controller.ex
@@ -2,8 +2,12 @@ defmodule EpisodeSchedulerWeb.CalendlyController do
     use EpisodeSchedulerWeb, :controller
 
     def create(conn, params) do
-        data = params
+        # send 400 if  payload is empty
+        if params == nil do
+            conn
+            |> put_status(400)
+            |> json(%{message: "No payload"})
+        end
         json(conn, %{message: "Webhook processed"})
     end
-
 end
--- test/episode_scheduler_web/controllers/calendly_controller_test.exs
+++ test/episode_scheduler_web/controllers/calendly_controller_test.exs
@@ -73,4 +73,10 @@ defmodule EpisodeSchedulerWeb.Controllers.CalendlyControllerTest do
     assert conn.status == 200
     refute json_response(conn, 200) == nil
   end
+
+  # test "POST /calendly responds with 400 given an empty payload", %{conn: conn} do
+  #   conn = post(conn, "/api/calendly", %{})
+  #   assert conn.status == 400
+  #   assert json_response(conn, 400)["message"] == "No payload"
+  # end
 end
