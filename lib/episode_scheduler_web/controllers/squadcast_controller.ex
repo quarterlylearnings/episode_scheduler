1489253be65b89c473bcd1711715286f2b6c7ed5
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-03 02:34:22 -0500

    Moves http logic downward

--- lib/episode_scheduler_web/controllers/squadcast_controller.ex
+++ lib/episode_scheduler_web/controllers/squadcast_controller.ex
@@ -1,7 +1,12 @@
 defmodule EpisodeSchedulerWeb.SquadcastController do
+  def http_client, do: Application.get_env(:episode_scheduler_web, :http_client)
   use EpisodeSchedulerWeb, :controller
 
-  def create(conn, params) do
+  def create_session(session) do
+    with {:ok, session} <- http_client().schedule_session(session) do
+      {:ok, session}
+    else
+      {:error, error} -> {:error, error}
+    end
   end
-
 end
