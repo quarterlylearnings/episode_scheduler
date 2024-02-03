3ea523ff7e920d6f0fa9970602d1158445055af1
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-03 01:46:22 -0500

    Adds mock for HTTPoison

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,6 @@
 ExUnit.start()
 # Start Mox so we can use it in our tests
-Mox.defmock(EpisodeSchedulerWeb.MockHTTPClient, for: EpisodeSchedulerWeb.Behaviours.HTTPClientBehaviour)
+Mox.defmock(HTTPClientBehaviourMock, for: EpisodeSchedulerWeb.Behaviours.HTTPClientBehaviour)
+Mox.defmock(HTTPMock, for: HTTPoison.Base)
 # Start the application
 Application.put_env(:episode_scheduler_web, :http_client, EpisodeSchedulerWeb.MockHTTPClient)
