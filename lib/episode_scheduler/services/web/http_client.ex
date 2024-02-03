a45d73349b3bda0a4f1ee64e438480bfcea77440
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-03 02:07:57 -0500

    Implements thin, single-responsibility http client

---
+++ lib/episode_scheduler/services/web/http_client.ex
@@ -0,0 +1,22 @@
+defmodule EpisodeScheduler.HTTPClient do
+  def http_adapter, do: Application.get_env(:episode_scheduler, :http_adapter)
+
+  # Create a session in SquadCast
+  def schedule_session(session) do
+    "/sessions"
+    |> send_post_request(session)
+  end
+
+  def send_post_request(url, body) do
+    http_adapter().post(url, body)
+    |> handle_response
+  end
+  defp handle_response(response) do
+    case response do
+      {:ok, %{body: response}} -> {:ok, response.body}
+      {:ok, %{body: response, status_code: 404}} -> {:error, response}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+end
