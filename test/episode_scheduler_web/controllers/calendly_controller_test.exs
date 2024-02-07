c19c9c148e2679cdde4df1f9f7d7871b0a31b409
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-07 14:59:21 -0500

    Refactors controller test

--- test/episode_scheduler_web/controllers/calendly_controller_test.exs
+++ test/episode_scheduler_web/controllers/calendly_controller_test.exs
@@ -5,13 +5,23 @@ defmodule EpisodeSchedulerWeb.Controllers.CalendlyControllerTest do
   import EpisodeSchedulerWeb.Constants
   import EpisodeSchedulerWeb.CalendlyController
 
-  @sample_payload get_test_calendly_event()
-
+  @test_calendly_event get_test_calendly_event()
+  @test_squadcast_response get_test_squadcast_response()
 
   test "POST /calendly responds with 200 given a valid payload", %{conn: conn} do
-    conn = create(conn, @sample_payload)
+    conn = create(conn, @test_calendly_event)
     assert conn.status == 200
     refute json_response(conn, 200) == nil
+  end
+
+  test "accepts an incoming webhook event from Calendly and prepares a SquadCast session payload" do
+    input = @test_calendly_event
+    output = @test_squadcast_response
+    # call method for creating squadcast request body from event payload
+    result = create_squadcast_request_body(input)
+    # assert the response contains the expected data
+    assert result == output
+
   end
 
   # test "POST /calendly responds with 400 given an empty payload", %{conn: conn} do
