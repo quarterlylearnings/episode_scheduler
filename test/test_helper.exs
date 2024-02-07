d85b2457447f20d8d3a02713fbc4ad682ab184b0
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-07 15:04:19 -0500

    Changes mock source

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,5 +2,4 @@ ExUnit.start()
 # Start Mox so we can use it in our tests
 Mox.defmock(HTTPClientBehaviourMock, for: EpisodeSchedulerWeb.Behaviours.HTTPClientBehaviour)
 Mox.defmock(HTTPMock, for: HTTPoison.Base)
-# Start the application
-Application.put_env(:episode_scheduler_web, :http_client, EpisodeSchedulerWeb.MockHTTPClient)
+Application.put_env(:episode_scheduler, :http_client, HTTPClientBehaviourMock)
