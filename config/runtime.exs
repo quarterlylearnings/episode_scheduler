d0eb952442cedcc2eda180fbd9f3a2e055070c81
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-08 16:11:07 -0500

    Adds runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :episode_scheduler, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -48,6 +48,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    server: true,
     secret_key_base: secret_key_base
 
   # ## SSL Support
