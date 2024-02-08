3ba3439366fa40d5a7ea6a63c7e7c368a760e2d4
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-08 16:19:32 -0500

    Modifies origin value for deployment

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,16 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :episode_scheduler, EpisodeSchedulerWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
+    check_origin: ["//*.gigalixirapp.com"],
+    server: true,
+    secret_key_base: secret_key_base
