0f9757cddd02d9bc03d80d6c7e03e161b7729008
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-08 12:09:33 -0500

    Changes structure of return value

--- test/episode_scheduler_web/controllers/squadcast_controller_test.exs
+++ test/episode_scheduler_web/controllers/squadcast_controller_test.exs
@@ -14,12 +14,12 @@ defmodule EpisodeSchedulerWeb.SquadcastControllerTest do
       HTTPClientBehaviourMock
       |> expect(:schedule_session, fn _body -> {:ok, @test_response} end)
       # create an assertion using HTTPMock
-      HTTPMock
-      |> expect(:post, fn _url, _body -> {:ok, @test_response} end)
+      # HTTPMock
+      # |> expect(:post, fn _url, _body -> {:ok, @test_response} end)
       # call the controller action
       result = SquadcastController.create_session(@test_request)
       # assert the response contains the expected data
-      assert result == @test_response
+      assert result == {@test_response}
 
     end
 
