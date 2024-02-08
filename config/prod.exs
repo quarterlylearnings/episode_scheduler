be1638443074e10b9e34a7c462f207f5acc41fde
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-02-08 16:54:49 -0500

    Changes prod config per Gigalixir docs

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ config :logger, level: :info
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
 config :episode_scheduler, EpisodeSchedulerWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", port: 443],
+  http: [port: System.get_env("PORT")],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
-  check_origin: ["//*.gigalixirapp.com"]
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
